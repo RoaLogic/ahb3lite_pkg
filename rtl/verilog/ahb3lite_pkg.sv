f454c8744464a1faa65e1a82341c36f373bd7bc4
Richard Herveille <richard.herveille@roalogic.com>
2021-10-13 16:56:36 +0200

    Added size parameters

--- rtl/verilog/ahb3lite_pkg.sv
+++ rtl/verilog/ahb3lite_pkg.sv
@@ -1,51 +1,48 @@
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    ██████╗  ██████╗  █████╗                                 //
-//    ██╔══██╗██╔═══██╗██╔══██╗                                //
-//    ██████╔╝██║   ██║███████║                                //
-//    ██╔══██╗██║   ██║██╔══██║                                //
-//    ██║  ██║╚██████╔╝██║  ██║                                //
-//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
-//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
-//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
-//          ██║     ██║   ██║██║  ███╗██║██║                   //
-//          ██║     ██║   ██║██║   ██║██║██║                   //
-//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
-//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
-//                                                             //
-//    AMBA AHB3-Lite Package                                   //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    Copyright (C) 2015-2016 ROA Logic BV                     //
-//    www.roalogic.com                                         //
-//                                                             //
-//   This source file may be used and distributed without      //
-// restriction provided that this copyright statement is not   //
-// removed from the file and that any derivative work contains //
-// the original copyright notice and the associated disclaimer.//
-//                                                             //
-//     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     //
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   //
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   //
-// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      //
-// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         //
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    //
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   //
-// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        //
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  //
-// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  //
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  //
-// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         //
-// POSSIBILITY OF SUCH DAMAGE.                                 //
-//                                                             //
-/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//    AMBA AHB3-Lite Package                                       //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2015-2021 Roa Logic BV                //
+//             www.roalogic.com                                    //
+//                                                                 //
+//     This source file may be used and distributed without        //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR        //
+//   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,           //
+//   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES      //
+//   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE     //
+//   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR          //
+//   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    //
+//   LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT    //
+//   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT    //
+//   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           //
+//   POSSIBILITY OF SUCH DAMAGE.                                   //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
 
  
 /************************************************
  * AHB3 Lite Package
  */
 package ahb3lite_pkg;
+  parameter       HTRANS_SIZE   = 2;
+  parameter       HSIZE_SIZE    = 3;
+  parameter       HBURST_SIZE   = 3;
+  parameter       HPROT_SIZE    = 4;
+
   //HTRANS
   parameter [1:0] HTRANS_IDLE   = 2'b00,
                   HTRANS_BUSY   = 2'b01,
